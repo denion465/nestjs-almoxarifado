a1c76f1c99734e94dacea5b1bd6cb8d750f2a1de
Daniel Vidal <danielsv465@hotmail.com>
2022-05-25 18:22:07 -0300

    Testes unitários de listagem e de criação de produto

--- src/almoxarifado/produto/services/produto.service.spec.ts
+++ src/almoxarifado/produto/services/produto.service.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ProdutoRepository } from '../repository/produto.repository';
 import { ProdutoService } from './produto.service';
 
 describe('ProdutoService', () => {
   let service: ProdutoService;
+  let repository: ProdutoRepository;
+
+  const mockProductRepository = () => ({
+    findAll: jest.fn(),
+    create: jest.fn()
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProdutoService]
+      providers: [
+        ProdutoService,
+        {
+          provide: ProdutoRepository,
+          useFactory: mockProductRepository
+        }
+      ]
     }).compile();
 
     service = module.get<ProdutoService>(ProdutoService);
+    repository = module.get<ProdutoRepository>(ProdutoRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be list all products', async () => {
+    const createdProduct = {
+      id: 'abe5d5a1-d9ae-4909-aaa7-7bd0f112ece4',
+      descricao: 'description test',
+      observacao: 'observation test',
+      data_atualizacao: new Date(),
+      data_cadastro: new Date()
+    };
+
+    jest
+      .spyOn(repository, 'findAll')
+      .mockImplementation(async () => [createdProduct]);
+
+    const result = await service.findAll();
+
+    expect(result).toEqual([createdProduct]);
+  });
+
+  it('should be create a new product', async () => {
+    const productToBeCreated = {
+      descricao: 'description test',
+      observacao: 'observation test'
+    };
+
+    const createdProduct = {
+      id: 'abe5d5a1-d9ae-4909-aaa7-7bd0f112ece4',
+      descricao: 'description test',
+      observacao: 'observation test',
+      data_atualizacao: new Date(),
+      data_cadastro: new Date()
+    };
+
+    jest
+      .spyOn(repository, 'create')
+      .mockReturnValue(Promise.resolve(createdProduct));
+
+    expect(await service.create(productToBeCreated)).toBe(createdProduct);
   });
 });
