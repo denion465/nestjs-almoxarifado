5661f6db65b366ac6ef859026926c5b6585aac3d
Daniel Vidal <danielsv465@hotmail.com>
2022-05-25 23:15:48 -0300

    Registrando novas entidades

--- src/almoxarifado/produto/produto.module.ts
+++ src/almoxarifado/produto/produto.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Produto } from './entities/produto.entity';
+import { Entrada } from './entities/entrada.entity';
+import { EntradaProduto } from './entities/entrada.produto.entity';
+import { Estoque } from './entities/estoque.entity';
+
 import { ProdutoController } from './controllers/produto.controller';
 import { ProdutoRepository } from './repository/produto.repository';
 import { ProdutoService } from './services/produto.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Produto])],
+  imports: [
+    TypeOrmModule.forFeature([Produto, Entrada, EntradaProduto, Estoque])
+  ],
   controllers: [ProdutoController],
   providers: [ProdutoRepository, ProdutoService]
 })
